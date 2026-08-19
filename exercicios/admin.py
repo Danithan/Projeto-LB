5a6b172d0a24bed8e8a013228b204a6addc25cd7
Guilherme-Leite1701 <guilhermelimaleite1324@gmail.com>
2026-08-19 18:25:49 -0300

    feat: Adição das permissões de administrador dos modelos

--- exercicios/admin.py
+++ exercicios/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ExercicioModelo, ExercicioResultado
 
-# Register your models here.
+admin.site.register(ExercicioModelo)
+admin.site.register(ExercicioResultado)
