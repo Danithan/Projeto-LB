544addcfcf471b02bea69a603340c26d8b159c1c
Guilherme-Leite1701 <guilhermelimaleite1324@gmail.com>
2026-08-19 18:04:51 -0300

    feat: Adição dos seguintes modelos e migrations: -Sessoes -Exercicios

--- sessoes/models.py
+++ sessoes/models.py
@@ -1,7 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from criancas.models import Crianca
 
 
 class SessaoModelo(models.Model):
     numero = models.IntegerField(unique=True)
     titulo = models.CharField(max_length=50)
     descricao = models.TextField(blank=True)
+
+class SessaoRealizada(models.Model):
+    STATUS_CHOICES = [
+        ('em_andamento', 'Em Andamento'),
+        ('concluida', 'Concluída'),
+    ]
+
+    crianca = models.ForeignKey(Crianca, on_delete=models.CASCADE)
+    sessao_modelo = models.ForeignKey(SessaoModelo, on_delete=models.PROTECT)
+    terapeuta = models.ForeignKey(User, on_delete=models.PROTECT)
+    data = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    observacoes = models.TextField(blank=True)
